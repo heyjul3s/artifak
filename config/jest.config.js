7d1f96681285aeb5f1df9ec2fb8b3104365430b1
heyjul3s <julian.cy.low@gmail.com>
2020-08-11 11:00:05 +1000

    update jest config

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,18 +1,13 @@
 module.exports = {
   rootDir: '../',
   collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts, tsx, js,jsx}',
-    '!<rootDir>/.storybook/*',
-    '!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}',
-  ],
-  moduleDirectories: ['node_modules', 'bower_components'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
   transform: {
-    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
     '^.+\\.(tsx|ts)?$': 'ts-jest',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx|ts|tsx)$',
-  verbose: false,
 };
