428f0ad7f2a509438e6a75970a1fe084657fa01d
heyjul3s <julian.cy.low@gmail.com>
2020-08-11 22:12:07 +1000

    index grid update

--- packages/grid/index.tsx
+++ packages/grid/index.tsx
@@ -1,18 +1,2 @@
-import React from 'react';
-import { GridSystem } from './GridSystem';
-import { TGridProps } from './types';
-import { createStyledComponent } from '@react-artifact/component-generator';
-
-const GridStyles = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
-  gridTemplateRows: 'repeat(3, 320px)',
-  gridGap: '1.5rem',
-  padding: '1.5rem 0',
-};
-
-export const Grid: React.FC = createStyledComponent<Partial<TGridProps>>(
-  GridStyles,
-  'GRID',
-  GridSystem
-);
+export { Grid } from './Grid';
+export { GridItem } from './GridItem';
