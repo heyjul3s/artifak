9cf0afbd6e679804d81dd905e9e964f05b64386d
heyjul3s <julian.cy.low@gmail.com>
2020-08-13 11:30:54 +1000

    update Grid component, rename types to typings, add Grid MDX

--- packages/grid/Grid.tsx
+++ packages/grid/Grid.tsx
@@ -1,18 +1,108 @@
-import React from 'react';
-import { GridSystem } from './GridSystem';
-import { TGridProps } from './types';
-import { createStyledComponent } from '@react-artifact/component-generator';
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+import { TGridProps } from './typings';
 
-const GridStyles = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
-  gridTemplateRows: 'repeat(3, 320px)',
-  gridGap: '1.5rem',
-  padding: '1.5rem 0',
+import {
+  compose,
+  display,
+  grid,
+  justifyItems,
+  justifyContent,
+  alignItems,
+  alignContent,
+  space,
+} from 'styled-system';
+
+// TODO: themes?
+// TODO: variants?
+
+const setGridColumnsAndRows = (
+  columnWidth,
+  columnLength,
+  rowLength,
+  rowHeight
+) => {
+  return {
+    ...gridTemplateColumns(columnWidth, columnLength),
+    ...gridTemplateRows(rowLength, rowHeight),
+  };
 };
 
-export const Grid: React.FC = createStyledComponent<Partial<TGridProps>>(
-  GridStyles,
-  'GRID',
-  GridSystem
+const gridTemplateColumns = (
+  columnWidth: string,
+  columnLength: number
+): object => {
+  if (columnWidth) {
+    return setRepeatableColumnWidth(columnWidth);
+  }
+
+  if (columnLength) {
+    return setRepeatableColumnLength(columnLength);
+  }
+
+  return {};
+};
+
+const setRepeatableColumnWidth = (columnWidth: string): object => ({
+  gridTemplateColumns: `repeat(auto-fit, minmax(${columnWidth}, 1fr))`,
+});
+
+const setRepeatableColumnLength = (columnLength: number): object => ({
+  gridTemplateColumns: `repeat(${columnLength}, 1fr)`,
+});
+
+const gridTemplateRows = (rowLength, rowHeight) =>
+  rowLength && rowHeight ? setGridTemplateRows(rowLength, rowHeight) : {};
+
+const setGridTemplateRows = (rowLength, rowHeight) => ({
+  gridTemplateRows: `repeat(${rowLength}, ${rowHeight})`,
+});
+
+export const StyledGrid = styled('div')<TGridProps>(
+  ({ columnWidth, columnLength, rowHeight, rowLength, gap = '1.5rem' }) => ({
+    display: 'grid',
+    gridGap: `${gap}`,
+    ...setGridColumnsAndRows(columnWidth, columnLength, rowHeight, rowLength),
+  }),
+
+  compose(
+    display,
+    grid,
+    space,
+    alignItems,
+    alignContent,
+    justifyItems,
+    justifyContent
+  )
+);
+
+type TSomeGrid = {
+  columnLength: number;
+  rowLength: number;
+  columnWidth: number;
+  rowHeight: number;
+  gap: number;
+};
+
+export const Grid = forwardRef(
+  (
+    {
+      columnLength,
+      columnWidth,
+      rowLength,
+      rowHeight,
+      gap,
+      ...props
+    }: TSomeGrid,
+    ref
+  ) => (
+    <StyledGrid
+      columnWidth={columnWidth}
+      columnLength={columnLength}
+      rowLength={rowLength}
+      rowHeight={rowHeight}
+      ref={ref}
+      {...props}
+    />
+  )
 );
