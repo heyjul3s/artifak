ac35b911937f82b823dbd66495068d059d23bd0e
heyjul3s <julian.cy.low@gmail.com>
2020-08-13 12:16:36 +1000

    update grid item component

--- packages/grid/GridItem.tsx
+++ packages/grid/GridItem.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { GridItemSystem } from './GridSystem';
-import { TGridItemProps } from './types';
-import { createStyledComponent } from '@react-artifact/component-generator';
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+import { TGridItemProps } from './typings';
 
-export const GridItem: React.FC = createStyledComponent<
-  Partial<TGridItemProps>
->({}, 'GRID_ITEM', GridItemSystem);
+import {
+  compose,
+  gridArea,
+  gridColumn,
+  gridRow,
+  justifySelf,
+  alignSelf,
+  maxHeight,
+  maxWidth,
+  minHeight,
+  minWidth,
+  space,
+} from 'styled-system';
+
+export const StyledGridItem = styled.div<TGridItemProps>(
+  compose(
+    gridArea,
+    gridRow,
+    gridColumn,
+    alignSelf,
+    justifySelf,
+    space,
+    maxHeight,
+    maxWidth,
+    minHeight,
+    minWidth
+  )
+);
+
+export const GridItem = forwardRef((props, ref) => (
+  <StyledGridItem ref={ref} {...props} />
+));
