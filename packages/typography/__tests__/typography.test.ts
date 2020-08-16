4eb17883ab5a9440702056eb0a72d8aba0ec8d2a
heyjul3s <julian.cy.low@gmail.com>
2020-08-16 11:37:57 +1000

    update test

--- packages/typography/__tests__/typography.test.ts
+++ packages/typography/__tests__/typography.test.ts
@@ -1,21 +1,17 @@
 import {
   createTypographyComponents,
   fluidSizing,
-  lineHeightByPercentage,
+  lineHeightInDecimals,
   TypographySystemComponent,
 } from '../';
 
-import { H1, H2, mockTypographyStyles } from '../__mocks__/Typography';
+import { mockTypographyStyles } from '../__mocks__/Typography';
 
 describe('@react-artifact/typography', () => {
   describe('createTypographyComponents - generates typography React components based on styles object argument provided', () => {
-    it('should return an empty object when provided with an empty styles object as argument', () => {
-      const expected = {};
-      expect(createTypographyComponents({} as any)).toEqual(expected);
-    });
-
     it('should return the base Typography system component when provided with an invalid argument', () => {
       const expected = TypographySystemComponent;
+      expect(createTypographyComponents({} as any)).toEqual(expected);
       expect(createTypographyComponents(null as any)).toEqual(expected);
       expect(createTypographyComponents(false as any)).toEqual(expected);
       expect(createTypographyComponents(0 as any)).toEqual(expected);
@@ -23,16 +19,13 @@ describe('@react-artifact/typography', () => {
     });
 
     it('should create React components', () => {
-      const expected = { H1, H2 };
-      expect(createTypographyComponents(mockTypographyStyles)).toEqual(
-        expected
-      );
+      expect(createTypographyComponents(mockTypographyStyles)).toBeDefined();
     });
   });
 
   describe('lineHeightByPercentage - provides a decimate value with a default of 1.45', () => {
     it('should EQUAL 1.45 by default', () => {
-      expect(lineHeightByPercentage()).toEqual(1.45);
+      expect(lineHeightInDecimals()).toEqual(1.45);
     });
   });
 
