abb668c99dc18d575c48f3ae0c33a5e587efd96b
heyjul3s <julian.cy.low@gmail.com>
2020-08-20 12:43:56 +1000

    split functions into diff files and export from single barrel

--- packages/typography/index.tsx
+++ packages/typography/index.tsx
@@ -1,61 +1,7 @@
-import isPlainObject from 'lodash.isplainobject';
-import styled from 'styled-components';
-import { borderRadius, compose, color, space, typography } from 'styled-system';
-import { createComponents } from '@artifak/component-generator';
-import { TypographySystem } from './typings';
+export {
+  createTypographyComponents,
+  TypographyBase,
+} from './createTypographyComponents';
 
-export const Typography = styled('div')(
-  compose(borderRadius, color, space, typography)
-);
-
-export function createTypographyComponents<ST>(systemComponentStyles: ST) {
-  if (
-    isPlainObject(systemComponentStyles) &&
-    Object.keys(systemComponentStyles).length
-  ) {
-    return createComponents<
-      ST,
-      { [key in keyof ST]: React.FC<TypographySystem> }
-    >(Typography, systemComponentStyles);
-  } else {
-    return Typography;
-  }
-}
-
-export function fluidSizing(
-  minFontSize: number,
-  maxFontSize: number,
-  minViewportWidth: number,
-  maxViewportWidth: number
-) {
-  return (
-    !!minFontSize &&
-    !!maxFontSize &&
-    !!minViewportWidth &&
-    !!maxViewportWidth &&
-    `calc(${minFontSize}px + (${maxFontSize} - ${minFontSize}) * ((100vw - ${minViewportWidth}px) / (${maxViewportWidth} - ${minViewportWidth})))`
-  );
-}
-
-enum EFontWeightKeys {
-  THIN = 'thin',
-  EXTRA_LIGHT = 'extraLight',
-  LIGHT = 'light',
-  REGULAR = 'regular',
-  MEDIUM = 'medium',
-  SEMI_BOLD = 'semiBold',
-  BOLD = 'bold',
-  EXTRA_BOLD = 'extraBold',
-  BLACK = 'black',
-}
-
-type TFontWeight = {
-  [key in EFontWeightKeys]: number;
-};
-
-export const fontWeight = Object.keys(EFontWeightKeys).reduce((acc, key, i) => {
-  const index = i === 0 ? 1 : i;
-  acc[key] = index * 100;
-
-  return acc;
-}, {} as TFontWeight);
+export { fontWeight } from './fontWeight';
+export { fluidSizing } from './fluidSizing';
