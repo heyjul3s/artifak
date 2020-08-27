a8bd27851cbb276f5ca9f53abf84d236aeae2618
heyjul3s <julian.cy.low@gmail.com>
2020-08-27 21:50:22 +1000

    update Syntax, add Params renderer

--- docs/components/Syntax.tsx
+++ docs/components/Syntax.tsx
@@ -1,14 +1,69 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import coy from 'react-syntax-highlighter/dist/cjs/styles/prism/coy';
+import { SmallParagraph, Strong } from '../components/Typography';
 
-export function Syntax({ children }) {
+const customCoy = {
+  ...coy,
+  'code[class*="language-"]': {
+    ...coy['code[class*="language-"]'],
+    fontFamily: null
+  },
+  'pre[class*="language-"]': {
+    ...coy['pre[class*="language-"]'],
+    fontFamily: null
+  }
+};
+
+type Props = {
+  language?: string;
+  showLineNumbers?: boolean;
+  children: React.ReactChild | React.ReactChildren;
+};
+
+export function Syntax({
+  children,
+  showLineNumbers = true,
+  language = 'typescript'
+}: Props) {
   return (
     <SyntaxHighlighter
-      language="typescript"
-      style={atomDark}
-      customStyle={{ padding: '1em', margin: '1em 0 2em' }}
+      showLineNumbers={showLineNumbers}
+      language={language}
+      style={customCoy}
+      customStyle={{
+        backgroundColor: '#fafafa',
+        borderRadius: 4,
+        fontSize: 13,
+        overflowX: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        padding: '1em',
+        margin: '1em 0 2em'
+      }}
     >
       {children}
     </SyntaxHighlighter>
   );
 }
+
+export const Params = ({ properties }: any) => {
+  const propertyKeys = Object.keys(properties);
+
+  return (
+    <>
+      <SmallParagraph>
+        (
+        {propertyKeys.map((key, i) => {
+          const comma = i !== propertyKeys.length - 1 ? ', ' : '';
+
+          return (
+            <span>
+              <Strong>{key}</Strong>
+              {`: ${properties[key]}${comma}`}
+            </span>
+          );
+        })}
+        )
+      </SmallParagraph>
+    </>
+  );
+};
