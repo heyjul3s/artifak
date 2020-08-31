fd11e04d5710afa2eea7bbd8bf7773060f960ec0
heyjul3s <julian.cy.low@gmail.com>
2020-08-31 22:44:20 +1000

    remove unused types for Block

--- packages/block/typings.ts
+++ packages/block/typings.ts
@@ -6,18 +6,9 @@ import {
   LayoutProps,
 } from 'styled-system';
 
-import { StyledComponent } from 'styled-components';
-
 export type BlockBaseProps =
   | LayoutProps
   | BorderRadiusProps
   | ColorProps
   | TypographyProps
   | SpaceProps;
-
-export type StyledReactComponent = StyledComponent<
-  keyof JSX.IntrinsicElements | React.ComponentType<any>,
-  any,
-  any,
-  any
->;
