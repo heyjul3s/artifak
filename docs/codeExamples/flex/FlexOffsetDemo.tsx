d18c8cb20bb9755052166e1ca9d7652bab4a5640
heyjul3s <julian.cy.low@gmail.com>
2020-09-01 21:13:18 +1000

    demo fix

--- docs/codeExamples/flex/FlexOffsetDemo.tsx
+++ docs/codeExamples/flex/FlexOffsetDemo.tsx
@@ -6,55 +6,75 @@ export function FlexOffsetDemo() {
   return (
     <FlexRow>
       <FlexCol columnSize={12} offset={0}>
-        <Block />
+        <Block>
+          <span>0</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={11} offset={1}>
-        <Block />
+        <Block>
+          <span>1</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={10} offset={2}>
-        <Block />
+        <Block>
+          <span>2</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={9} offset={3}>
-        <Block />
+        <Block>
+          <span>3</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={8} offset={4}>
-        <Block />
+        <Block>
+          <span>4</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={7} offset={5}>
-        <Block />
+        <Block>
+          <span>5</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={6} offset={6}>
-        <Block />
+        <Block>
+          <span>6</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={5} offset={7}>
-        <Block />
+        <Block>
+          <span>7</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={4} offset={8}>
-        <Block />
+        <Block>
+          <span>8</span>
+        </Block>
       </FlexCol>
 
-      <FlexCol columnSize={4} offset={9}>
-        <Block />
-      </FlexCol>
-
-      <FlexCol columnSize={3} offset={10}>
-        <Block />
+      <FlexCol columnSize={3} offset={9}>
+        <Block>
+          <span>9</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={2} offset={10}>
-        <Block />
+        <Block>
+          <span>10</span>
+        </Block>
       </FlexCol>
 
       <FlexCol columnSize={1} offset={11}>
-        <Block />
+        <Block>
+          <span>11</span>
+        </Block>
       </FlexCol>
     </FlexRow>
   );
@@ -66,5 +86,14 @@ const Block = styled.div`
     width: 100%;
     height: 40px;
     background: ${theme.colors.primary};
+    margin-bottom: 5px;
+
+    > span {
+      display: block;
+      color: white;
+      padding: 10px;
+      text-transform: uppercase;
+      line-height: 1.5;
+    }
   `}
 `;
