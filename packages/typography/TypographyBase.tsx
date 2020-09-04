2ef204d08c3556d5a62eb83d308c60d8d3ab43f6
heyjul3s <julian.cy.low@gmail.com>
2020-09-04 12:02:00 +1000

    Extend Typography system - added csstype pkg - declared new typography interfaces - added custom styles config for extension of typography system

--- packages/typography/TypographyBase.tsx
+++ packages/typography/TypographyBase.tsx
@@ -1,3 +1,28 @@
+import { system } from 'styled-system';
 import { createStyledComponent } from '@artifak/component-generator';
 
-export const TypographyBase = createStyledComponent<{}>();
+const typographyStylesConfig = system({
+  textDecoration: {
+    property: 'textDecoration',
+  },
+  textIndent: {
+    property: 'textIndent',
+  },
+  textTransform: {
+    property: 'textTransform',
+  },
+  textOverflow: {
+    property: 'textOverflow',
+  },
+  whiteSpace: {
+    property: 'whiteSpace',
+  },
+  wordBreak: {
+    property: 'wordBreak',
+  },
+  wordSpacing: {
+    property: 'wordSpacing',
+  },
+});
+
+export const TypographyBase = createStyledComponent([typographyStylesConfig]);
