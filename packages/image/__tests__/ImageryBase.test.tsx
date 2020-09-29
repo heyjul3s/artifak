0c34b2b78d806696bd0a5b182006b2ad6630061d
heyjul3s <julian.cy.low@gmail.com>
2020-09-29 12:08:04 +1000

    update ImageryBase test

---
+++ packages/image/__tests__/ImageryBase.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ImageryBase } from '../ImageryBase';
+import 'jest-styled-components';
+
+describe('ImageryBase', () => {
+  it('should render', () => {
+    const { asFragment } = render(<ImageryBase />, {});
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render styles object-fit and object-position', () => {
+    const { asFragment, container } = render(
+      <ImageryBase objectFit="cover" objectPosition="center" />,
+      {}
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule('object-fit', 'cover');
+    expect(container.firstChild).toHaveStyleRule('object-position', 'center');
+  });
+});
