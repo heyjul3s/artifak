c0cd40ea8b47c87f39f60f83b963fac7b5a97c77
heyjul3s <julian.cy.low@gmail.com>
2020-10-24 16:54:45 +1100

    Config Updates - add eslintignore - add single quote rule to eslint - move prettier.config to root - add esw pkg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -11,15 +11,16 @@ module.exports = {
     'prettier/@typescript-eslint',
     // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:prettier/recommended',
-    'prettier/react',
+    'prettier/react'
   ],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   rules: {
+    quotes: [2, 'single', { avoidEscape: true }]
     // place to specify ESLint rules - can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
+  }
 };
