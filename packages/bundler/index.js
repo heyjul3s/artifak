f2f4247dd0b500d222aa26009f7af4e5456e56b1
heyjul3s <julian.cy.low@gmail.com>
2020-10-25 11:15:03 +1100

    Bundler FIx - fix unable to find rollupBabelHelpers.js error due to babel-plugin-styled-components - bring back ts styled-components transformer

--- packages/bundler/index.js
+++ packages/bundler/index.js
@@ -5,99 +5,94 @@ const resolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('@rollup/plugin-babel').default;
 const typescript = require('rollup-plugin-typescript2');
 const commonjs = require('@rollup/plugin-commonjs');
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components')
+  .default;
 const currentWorkingPath = process.cwd();
 
-const {
-  src,
-  main: { pkgMain },
-  module: { pkgModule },
-  name,
-} = require(path.join(currentWorkingPath, 'package.json'));
+const { src, name } = require(path.join(currentWorkingPath, 'package.json'));
 
 const tsConfigPath = path.join(currentWorkingPath, 'tsconfig.json');
 const inputPath = path.join(currentWorkingPath, src);
 const fileName = name.replace('@artifak/', '');
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
+const styledComponentsTransformer = createStyledComponentsTransformer({
+  getDisplayName(filename, bindingName) {
+    return path.relative(__dirname, filename);
+  }
+});
+
 const inputOptions = {
   input: inputPath,
-  external: ['react', 'react-dom', 'styled-components', 'styled-system' ],
+  external: ['react', 'react-dom', 'styled-components', 'styled-system'],
   plugins: [
     resolve({
-      extensions,
+      extensions
     }),
     commonjs(),
     babel({
       extensions,
-      plugins: [
-        [
-          'babel-plugin-styled-components',
-          {
-            namespace: `artifak-${fileName}`,
-            displayName: true,
-          },
-        ],
-      ],
+      exclude: ['node_modules/**', 'src/__mocks__/**', 'src/__tests__/**'],
+      plugins: ['@babel/plugin-transform-runtime'],
       presets: [
         '@babel/preset-env',
         '@babel/preset-react',
-        '@babel/preset-typescript',
+        '@babel/preset-typescript'
       ],
-      babelHelpers: 'bundled',
+      babelHelpers: 'runtime'
     }),
     typescript({
       tsconfig: tsConfigPath,
-    }),
-  ],
+      transformers: [
+        () => ({
+          before: [styledComponentsTransformer]
+        })
+      ]
+    })
+  ]
+};
+
+const baseOutputOptions = {
+  name: `${fileName}`,
+  globals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'styled-components': 'StyledComponents',
+    'styled-system': 'StyledSystem'
+  },
+  sourcemap: true,
+  exports: 'named'
 };
 
 const outputOptions = [
   {
     file: `dist/${fileName}.cjs.js`,
     format: 'cjs',
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'styled-components': 'StyledComponents',
-      'styled-system': 'StyledSystem'
-    },
-    sourcemap: true,
-    exports: "named",
+    ...baseOutputOptions
   },
   {
     file: `dist/${fileName}.esm.js`,
     format: 'esm',
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'styled-components': 'StyledComponents',
-      'styled-system': 'StyledSystem'
-    },
-    sourcemap: true,
-    exports: "named",
+    ...baseOutputOptions
   },
   {
-    file: `dist/${fileName}.min.js`,
-    format: "umd",
-    name: `${fileName}`,
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'styled-components': 'StyledComponents',
-      'styled-system': 'StyledSystem'
-    },
+    file: `dist/${fileName}.umd.js`,
+    format: 'umd',
     esModule: false,
-    exports: "named",
-    sourcemap: true,
+    ...baseOutputOptions
   }
 ];
 
 async function build() {
-  const bundle = await rollup.rollup(inputOptions);
+  try {
+    const bundle = await rollup.rollup(inputOptions);
 
-  outputOptions.forEach(async (options) => {
-    await bundle.write(options);
-  });
+    outputOptions.forEach(async options => {
+      await bundle.write(options);
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
 build();
