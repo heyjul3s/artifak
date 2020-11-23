8b6c6ebdbf1a5aac60d5b2f64ef2f470d6ce0ccc
heyjul3s <julian.cy.low@gmail.com>
2020-11-23 12:40:49 +1100

    remove comments

--- packages/component-generator/src/createBaseComponents.tsx
+++ packages/component-generator/src/createBaseComponents.tsx
@@ -3,7 +3,6 @@ import { AnyStyledComponent } from 'styled-components';
 import { ComponentVariant } from './typings';
 
 export type Settings<A> = {
-  // TODO: types not strict enough
   styles: { [key in keyof any]: string | string[] | number | number[] };
   attrs?: A;
 };
