b02aa1d3448506d1546146fb07e4d3779d54ad5c
heyjul3s <julian.cy.low@gmail.com>
2020-12-07 11:55:19 +1100

    export types, change utils to use fn

--- packages/media/src/utils.ts
+++ packages/media/src/utils.ts
@@ -1,5 +1,7 @@
-export const isString = (arg: unknown): boolean =>
-  Object.prototype.toString.call(arg) === '[object String]';
+export function isString(arg: unknown): boolean {
+  return Object.prototype.toString.call(arg) === '[object String]';
+}
 
-export const isNonEmptyString = (arg?: unknown): boolean =>
-  isString(arg) && (arg as string).trim() !== '';
+export function isNonEmptyString(arg?: unknown): boolean {
+  return isString(arg) && (arg as string).trim() !== '';
+}
