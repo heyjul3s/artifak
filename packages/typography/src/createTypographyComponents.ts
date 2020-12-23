81007a0a0eb4932ecf6b938b04a5813f0f1e5aa8
heyjul3s <julian.cy.low@gmail.com>
2020-12-23 16:31:29 +1100

    Typo Update - update to use new Generator API - udpate tests - update exports - remove unused mocks

--- packages/typography/src/createTypographyComponents.ts
+++ packages/typography/src/createTypographyComponents.ts
@@ -1,18 +1,53 @@
-import { createBaseComponents } from '@artifak/component-generator';
+import {
+  GenericRecord,
+  createBaseComponents,
+  Settings,
+  StyledComponentConfig,
+  Variant
+} from '@artifak/component-generator';
 import { TypographyBaseProps } from './typings';
 import { typographyStyleProps } from './TypographyBase';
 
 export function createTypographyComponents<
   Config,
-  Theme = any,
-  Props = Record<string, unknown> & TypographyBaseProps,
+  ThemeType = any,
+  Props = {},
   Element = HTMLDivElement
->(settings: any) {
-  return createBaseComponents<Config, Theme, Props, Element>({
-    Base: {
-      ...settings.Base,
-      styleProps: [typographyStyleProps, ...settings.Base.styleProps]
+>(
+  base: StyledComponentConfig<Props, ThemeType, Element>,
+  settings: Settings
+): GenericRecord<
+  Config,
+  React.FC<Props & TypographyBaseProps & Variant<ThemeType>>
+> {
+  const { styleProps } = getStyleProps(base);
+
+  return createBaseComponents<
+    Config,
+    ThemeType,
+    Props & TypographyBaseProps,
+    Element
+  >(
+    {
+      ...base,
+      styleProps: [typographyStyleProps, ...styleProps]
     },
-    ...settings
-  });
+    settings
+  );
+}
+
+export function getStyleProps<
+  ThemeType = any,
+  Props = {},
+  Element = HTMLDivElement
+>(base: StyledComponentConfig<Props, ThemeType, Element>) {
+  if (!base || !Object.keys(base).length) {
+    return { styleProps: [] };
+  }
+
+  if (!base.styleProps) {
+    return { ...base, styleProps: [] };
+  }
+
+  return base;
 }
