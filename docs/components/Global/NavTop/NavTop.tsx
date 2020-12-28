56ff7518fa7548ea903970a0c635f599dbf01ec4
heyjul3s <julian.cy.low@gmail.com>
2020-12-28 13:24:12 +1100

    add type logo to top nav

--- docs/components/Global/NavTop/NavTop.tsx
+++ docs/components/Global/NavTop/NavTop.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ArtifakTypeLogo } from '../../../assets/ArtifakTypeLogo';
 import { NavTopContainer, NavTopMenu, NavTopLink } from './styles';
 
 export function NavTop() {
   return (
     <NavTopContainer>
+      <ArtifakTypeLogo />
       <NavTopMenu>
         <NavTopLink href="https//github.com/heyjul3s/artifak">
           Github
