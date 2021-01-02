74019c74810acbd66a6bd14f2538af6c7b9d18c4
heyjul3s <julian.cy.low@gmail.com>
2021-01-02 11:48:19 +1100

    add custom props to story

--- packages/text-input/index.stories.tsx
+++ packages/text-input/index.stories.tsx
@@ -23,14 +23,25 @@ const inputs = {
   }
 };
 
+type BasicUsageProps = {
+  disabled?: boolean;
+  placeholder?: string;
+  onBlur?: () => void;
+};
+
 const { Base: InputText, InputSearch, InputUrl } = createTextInputs<
-  typeof inputs.components
+  typeof inputs.components,
+  BasicUsageProps
 >(inputs.base, inputs.components);
 
 export function BasicUsage() {
+  const onBlur = (text: string) => () => {
+    console.log('Blur', text);
+  };
+
   return (
     <>
-      <InputText />
+      <InputText onBlur={onBlur('Hello')} />
       <InputSearch />
       <InputUrl />
     </>
