4d4e67f4b3ee6ab721a1c4f12e555c5293abd763
heyjul3s <julian.cy.low@gmail.com>
2021-01-12 13:30:29 +1100

    test update

--- packages/fluidsizing/__tests__/fluidsizing.test.ts
+++ packages/fluidsizing/__tests__/fluidsizing.test.ts
@@ -4,17 +4,23 @@ describe('fluidSizing - writes sizing CSS rule that dynamically adapts to screen
   it('should return FALSE by default', () => {
     const expected = '';
     expect(
-      fluidSizing(void 0 as any, void 0 as any, void 0 as any, void 0 as any)
+      fluidSizing(
+        void 0 as any,
+        void 0 as any,
+        void 0 as any,
+        void 0 as any,
+        void 0 as any
+      )
     ).toEqual(expected);
   });
 
   it('should return FALSE by when provided falsy arguments', () => {
     const expected = '';
-    expect(fluidSizing(void 0 as any, 96, 300, 1200)).toEqual(expected);
+    expect(fluidSizing(void 0 as any, 96, 300, 1200, 'px')).toEqual(expected);
   });
 
   it('should return a CSS string rule', () => {
     const expected = `calc(48px + (96 - 48) * ((100vw - 300px) / (1200 - 300)))`;
-    expect(fluidSizing(48, 96, 300, 1200)).toEqual(expected);
+    expect(fluidSizing(48, 96, 300, 1200, 'px')).toEqual(expected);
   });
 });
