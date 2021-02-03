a6264bba4a8c29275d4cb1321cb9bca2817d197a
heyjul3s <julian.cy.low@gmail.com>
2021-02-03 16:16:47 +1100

    hexToRGB feat - test coverage - refactor hextorgb

--- packages/hextorgb/src/hextorgb.ts
+++ packages/hextorgb/src/hextorgb.ts
@@ -1,32 +1,43 @@
 import { RGBColor } from './typings';
 
 export function hexToRGBA(hex: string, alpha: number = 1): string | undefined {
-  const color = hexToRGB(hex);
+  if (!isValidHex(hex)) {
+    return void 0;
+  }
 
-  return !!color
-    ? `rgba(${color.r}, ${color.g}, ${color.b}, ${alpha})`
-    : void 0;
+  const color = hexToRGB(hex);
+  return `rgba(${color!.r}, ${color!.g}, ${color!.b}, ${alpha})`;
 }
 
 export function hexToRGB(hex: string): RGBColor | undefined {
-  const normalizedHexValue = normalizeHex(hex);
-  const hexValue = validateHex(normalizedHexValue);
+  if (!isValidHex(hex)) {
+    return void 0;
+  }
 
-  return hexValue
-    ? {
-        r: parseInt(hexValue[1], 16),
-        g: parseInt(hexValue[2], 16),
-        b: parseInt(hexValue[3], 16)
-      }
-    : void 0;
+  const normalizedHexValue = expandShorthandHex(hex);
+  const hexValue = splitHexToRGB(normalizedHexValue);
+
+  return {
+    r: parseInt(hexValue![1], 16),
+    g: parseInt(hexValue![2], 16),
+    b: parseInt(hexValue![3], 16)
+  };
 }
 
-export function validateHex(hex: string) {
-  const hexRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
-  return hexRegex.exec(hex);
+export function splitHexToRGB(hex: string) {
+  const HEX_REGEX = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+  return HEX_REGEX.exec(hex);
 }
 
-export function normalizeHex(hex: string): string {
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  return hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+export function expandShorthandHex(hex: string): string {
+  const SHORTHAND_REGEX = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  return hex.replace(
+    SHORTHAND_REGEX,
+    (match, r, g, b) => r + r + g + g + b + b
+  );
+}
+
+export function isValidHex(hex: string) {
+  const HEX_REGEX = /^#?([a-f\d]){3,6}$/i;
+  return HEX_REGEX.test(hex);
 }
