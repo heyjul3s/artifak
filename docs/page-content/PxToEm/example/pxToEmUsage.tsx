190353b85a306e2c58cb866c0621f1179c80025a
heyjul3s <julian.cy.low@gmail.com>
2021-02-12 11:14:29 +1100

    update pxtoEm and pxToRem exampels

--- docs/page-content/PxToEm/example/pxToEmUsage.tsx
+++ docs/page-content/PxToEm/example/pxToEmUsage.tsx
@@ -2,6 +2,9 @@ export const pxToEmUsage = `
   import { pxToEm } from 'artifak';
   import styled from 'styled-components';
 
+  // const EMvalue = pxtoRem('16px');
+  // console.log(EMvalue); // prints '1em'
+
   const Container = styled.div\`
     margin: 0 auto;
     width: 100%;
--- docs/page-content/PxToRem/example/pxToRemUsage.tsx
+++ docs/page-content/PxToRem/example/pxToRemUsage.tsx
@@ -2,6 +2,9 @@ export const pxToRemUsage = `
   import { pxToRem } from 'artifak';
   import styled from 'styled-components';
 
+  // const REMvalue = pxtoRem('48px');
+  // console.log(REMvalue); // prints '3rem'
+
   const Container = styled.div\`
     margin: 0 auto;
     width: 100%;
