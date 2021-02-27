56b05ab4a19b1768e13e09228927110cb584b0d9
heyjul3s <julian.cy.low@gmail.com>
2021-02-27 13:21:54 +1100

    add peerdependencies, useprevious implementation

--- packages/usepreviousvalue/src/index.ts
+++ packages/usepreviousvalue/src/index.ts
@@ -1,3 +1,11 @@
-export function usePreviousValue() {
-  // TODO
+import { useRef, useEffect } from 'react';
+
+export function usePreviousValue<Value extends unknown>(value: Value) {
+  const ref = useRef<Value>();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
 }
