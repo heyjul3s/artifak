777d0a39b0fa861c85224e496303f24a66aba233
heyjul3s <julian.cy.low@gmail.com>
2021-03-01 10:48:18 +1100

    add test

--- packages/usepreviousvalue/__tests__/usepreviousvalue.test.ts
+++ packages/usepreviousvalue/__tests__/usepreviousvalue.test.ts
@@ -1,7 +1,23 @@
+import { renderHook } from '@testing-library/react-hooks';
 import { usePreviousValue } from '../src';
 
+const usePreviousValueHook = () =>
+  renderHook(({ state }) => usePreviousValue(state), {
+    initialProps: { state: 0 }
+  });
+
 describe('@artifak/usepreviousvalue', () => {
-  it('needs tests', () => {
-    //
+  it('should be undefined initially', () => {
+    const { result } = usePreviousValueHook();
+    expect(result.current).toBeUndefined();
+  });
+
+  it('should return previous state value on rerender', async () => {
+    const { result, rerender } = usePreviousValueHook();
+    rerender({ state: 1 });
+    expect(result.current).toBe(0);
+
+    rerender({ state: 2 });
+    expect(result.current).toBe(1);
   });
 });
