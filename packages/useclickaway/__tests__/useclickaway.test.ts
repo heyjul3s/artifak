3fd902f593901aecfaea1d96e82fd8f8790c6886
heyjul3s <julian.cy.low@gmail.com>
2021-03-29 21:54:24 +1100

    implement useClickAway

--- packages/useclickaway/__tests__/useclickaway.test.ts
+++
@@ -1,7 +0,0 @@
-import { useClickAway } from '../src';
-
-describe('@artifak/useclickaway', () => {
-  it('needs tests', () => {
-    //
-  });
-});
--- packages/useclickaway/src/index.ts
+++ packages/useclickaway/src/index.ts
@@ -1,3 +1,18 @@
-export function useClickAway() {
-  // TODO
+import { useEffect, RefObject } from 'react';
+
+export function useClickAway(
+  ref: RefObject<HTMLElement | null>,
+  onClickAway: (e: Event) => void
+) {
+  const handleClick = (e: Event) => {
+    ref.current && !ref.current.contains(e.target as Node) && onClickAway(e);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return function unmountUseClickAway() {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [ref]);
 }
